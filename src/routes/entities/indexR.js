eec6c5a2f4d8ced2af6921988e4ffaf292e7a185
Dohuy5360 <loldh321@gmail.com>
2023-05-02 17:27:49 +0700

    Fix sql in dashboard page

--- src/routes/entities/indexR.js
+++ src/routes/entities/indexR.js
@@ -8,7 +8,9 @@ indexR.get("/table/records", (req, res) => {
 		`
 		SELECT relname as table, COALESCE(NULLIF(reltuples::bigint, -1), 0) as record
 		FROM pg_class 
-		WHERE relkind = 'r' AND relname NOT LIKE 'pg_%' AND relname NOT LIKE 'sql_%' 
+		WHERE relkind = 'r' 
+		AND relname NOT LIKE 'pg_%' 
+		AND relname NOT LIKE 'sql_%' 
 		AND relname not like '%tray'
 		ORDER BY record DESC
 		`,
@@ -24,11 +26,12 @@ indexR.get("/table/records", (req, res) => {
 indexR.get("/table/headquarter/employee/count", (req, res) => {
 	pool.query(
 		`
-		select headquarter_name, count(employee_id) as employee_count
-		from headquarter h, employee e
-		where e.headquarter_id = h.headquarter_id
-		group by headquarter_name
-		order by employee_count DESC
+		SELECT headquarter_name, COALESCE(count(employee_id)) AS employee_count
+		FROM headquarter h
+		LEFT JOIN employee e
+		ON e.headquarter_id = H.headquarter_id
+		GROUP BY headquarter_name
+		ORDER BY employee_count DESC
 		`,
 		(err, records) => {
 			if (err) {
